41e7c74749f65d4e0aefd4e4b68b171c6be6c0bd
lamis ghoualmi <gh.lamis@live.fr>
2023-11-29 09:00:49 -0500

    Update NLP-To-SQL.py

--- NLP-To-SQL.py
+++ NLP-To-SQL.py
@@ -1,33 +1,14 @@
-
-
-
 import openai
 import numpy as np
 import streamlit as st
+import time
 
 # Load OpenAI API key from secrets file
-openai.api_key=st.secrets["pass"]
+openai.api_key = st.secrets["pass"]
 
+st.sidebar.header('Natural language to SQL queries.')
 
-
-st.sidebar.header('Natural language to SQL queries. ')
-
-st.sidebar.image("sql.png", use_column_width=True)
-
-st.sidebar.write("""
-         ######  This app uses streamlit and OpenAI APIs to translate natural language queries into SQL statements with ease. Simply input your natural language query, and our app will generate the corresponding SQL statement for you to execute, saving you valuable time and effort.
-         """)
-st.sidebar.write("""
-         ######  Made by Lamis Ghoualmi  [LinkedIn](https://www.linkedin.com/in/lamisghoualmi/), [Github](https://lamisghoualmi.github.io/)
-         """)
-
-
-
-st.header('Translate natural language to SQL queries.')
-
-st.info('Example: Write a query that prints a list of employee names from the Employee table in alphabetical order', icon="ℹ️")
-query= st.text_input('Enter your text to generate SQL query', '')
-
+# ... (rest of your code remains unchanged)
 
 def generate_sql(query):
     model_engine = "text-davinci-002"
@@ -36,26 +17,32 @@ def generate_sql(query):
         f"{query}\n"
         f"SQL:"
     )
-    response = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        temperature=0,
-        max_tokens=150,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-        stop=["#", ";"]
-    )
-    return response.choices[0].text.strip()
 
+    retries = 3  # Number of retries
+    for attempt in range(retries):
+        try:
+            response = openai.Completion.create(
+                engine=model_engine,
+                prompt=prompt,
+                temperature=0,
+                max_tokens=150,
+                top_p=1.0,
+                frequency_penalty=0.0,
+                presence_penalty=0.0,
+                stop=["#", ";"]
+            )
+            return response.choices[0].text.strip()
+        except openai.error.RateLimitError as e:
+            st.warning(f"Rate limit exceeded. Retrying in {2**attempt} seconds...")
+            time.sleep(2**attempt)
+
+    st.error("Exceeded maximum number of retries. Please try again later.")
+    return None
 
 if st.button('Generate SQL script'):
-  if len(query) > 0:
-    Respo=generate_sql(query)
-    st.write(Respo)
-  else:
-    st.warning('Please enter at least two words. Example: list of customers where country is United states', icon="⚠️")
-
-  
-
-
+    if len(query) > 0:
+        Respo = generate_sql(query)
+        if Respo is not None:
+            st.write(Respo)
+    else:
+        st.warning('Please enter at least two words. Example: list of customers where country is United states', icon="⚠️")
